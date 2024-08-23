4d38910fd5c2a94a43a10e7650684bf0285eccc8
Eduardo Coelho <eduardo.coelho.210304@gmail.com>
2024-08-23 18:19:26 -0300

    Add files via upload

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,32 @@
+function operacoes(){
+    // Adição
+    let a1 = 2;
+    let a2 = 3;
+    let r1 = a1 + a2;
+
+    console.log(`Resultado da Adição entre ${a1} e ${a2} é ${r1}`);
+
+    // Subtração
+    let b1 = 10;
+    let b2 = 5;
+    let r2 = b1 - b2;
+
+    console.log(`Resultado da Subtração entre ${b1} e ${b2} é ${r2}`);
+
+    // Multiplicação
+    let c1 = 2.5;
+    let c2 = 2;
+    let r3 = c1 * c2;
+
+    console.log(`Resultado da Multiplicação entre ${c1} e ${c2} é ${r3}`);
+
+    // Divisão
+    let d1 = 25;
+    let d2 = 5;
+    let r4 = d1 / d2;
+
+    console.log(`Resultado da Divisão entre ${d1} e ${d2} é ${r4}`);
+}
+
+
+operacoes();
